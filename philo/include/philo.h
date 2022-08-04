0685cdcf20f4bd780680ed84feb95d7517e1063d
Guillaume KEHREN <gkehren@e3r4p12.clusters.42paris.fr>
2022-08-04 17:18:43 +0200

    42

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,15 +6,42 @@
 /*   By: gkehren <gkehren@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 15:39:59 by gkehren           #+#    #+#             */
-/*   Updated: 2022/08/03 15:46:19 by gkehren          ###   ########.fr       */
+/*   Updated: 2022/08/04 17:17:33 by gkehren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+/*******INCLUDE*******/
+
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <pthread.h>
+
+/*******DEFINE*******/
+// this define will be change to the initial number_of_philosophers
+# define INITIAL_FORK	0
+
+/*******STRUCT*******/
+
+typedef struct s_philo
+{
+	int				fork;
+	pthread_mutex_t	mutex_fork;
+	pthread_t		table;
+	pthread_t		*philos;
+}	t_philo;
+
+static t_philo store =
+{
+	.fork = INITIAL_FORK,
+	.mutex_fork = PTHREAD_MUTEX_INITIALIZER,
+};
+
+/*******UTILS*******/
+
+int	ft_atoi(const char *str);
 
 #endif
--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,15 +6,45 @@
 /*   By: gkehren <gkehren@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 15:40:19 by gkehren           #+#    #+#             */
-/*   Updated: 2022/08/03 15:45:50 by gkehren          ###   ########.fr       */
+/*   Updated: 2022/08/04 17:13:07 by gkehren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
+static void	*fn_table(void	*p_data)
+{
+	while (1)
+	{
+		/* beginning of the protected area */
+		pthread_mutex_lock(&store.mutex_fork);
+		if (store.fork <= 0)
+		{
+			store.fork = INITIAL_FORK;
+			printf("fill table with %d fork!\n", store.fork);
+		}
+		/* ending of the protected area */
+		pthread_mutex_unlock(&store.mutex_fork);
+	}
+	(void)p_data;
+	return (NULL);
+}
+
+void	init(t_philo *philo, int argc, char **argv)
+{
+	//int	i;
+	int	ret;
+
+	(void)argc;
+	philo->philos = malloc(sizeof(pthread_t) * ft_atoi(argv[1]));
+	printf("Threading table...\n");
+	ret = pthread_create(&philo->table, NULL, fn_table, NULL);
+}
+
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	t_philo	philo;
+
+	init(&philo, argc, argv);
 	return (0);
 }
