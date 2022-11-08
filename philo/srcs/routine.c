97cf68784049056b0ba85603cd8fe2057f429b59
gkehren <gkehren@student.42.fr>
2022-11-08 14:33:41 +0100

    [FIX] Data races and dead lock

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: gkehren <gkehren@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 17:18:33 by gkehren           #+#    #+#             */
-/*   Updated: 2022/11/07 01:46:36 by gkehren          ###   ########.fr       */
+/*   Updated: 2022/11/08 14:30:43 by gkehren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	meals_count(t_philo *philo)
 {
+	pthread_mutex_lock(&(philo->table->microphone));
 	if (!philo->table->waiter.close_the_place)
 	{
 		pthread_mutex_lock(&(philo->table->meals_count_access));
@@ -22,14 +23,25 @@ void	meals_count(t_philo *philo)
 			(philo->table->waiter.table_capacity)--;
 		pthread_mutex_unlock(&(philo->table->meals_count_access));
 	}
+	pthread_mutex_unlock(&(philo->table->microphone));
 }
 
 void	*eats(t_philo *philo)
 {
-	pthread_mutex_lock(philo->left_hand);
-	print_state(philo, HAS_TAKEN_A_FORK);
-	pthread_mutex_lock(philo->right_hand);
-	print_state(philo, HAS_TAKEN_A_FORK);
+	if (philo->philo_num % 2 == 0)
+	{
+		pthread_mutex_lock(philo->left_hand);
+		print_state(philo, HAS_TAKEN_A_FORK);
+		pthread_mutex_lock(philo->right_hand);
+		print_state(philo, HAS_TAKEN_A_FORK);
+	}
+	else
+	{
+		pthread_mutex_lock(philo->right_hand);
+		print_state(philo, HAS_TAKEN_A_FORK);
+		pthread_mutex_lock(philo->left_hand);
+		print_state(philo, HAS_TAKEN_A_FORK);
+	}
 	print_state(philo, EATING);
 	meals_count(philo);
 	usleep((philo->table->time_to_eat * 1000));
@@ -50,8 +62,10 @@ void	*routine(void *arg)
 	t_philo		*philo;
 
 	philo = arg;
+	pthread_mutex_lock(&(philo->table->microphone));
 	while (!philo->table->waiter.close_the_place)
 	{
+		pthread_mutex_unlock(&(philo->table->microphone));
 		if (philo->philo_num % 2 == 0)
 			usleep(300);
 		if (philo->table->num_of_philos == 1)
@@ -64,6 +78,8 @@ void	*routine(void *arg)
 		eats(philo);
 		sleeps(philo);
 		print_state(philo, THINKING);
+		pthread_mutex_lock(&(philo->table->microphone));
 	}
+	pthread_mutex_unlock(&(philo->table->microphone));
 	pthread_exit(EXIT_SUCCESS);
 }
--- philo/srcs/waiter.c
+++ philo/srcs/waiter.c
@@ -6,7 +6,7 @@
 /*   By: gkehren <gkehren@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 14:03:34 by gkehren           #+#    #+#             */
-/*   Updated: 2022/11/07 01:46:25 by gkehren          ###   ########.fr       */
+/*   Updated: 2022/11/08 14:27:19 by gkehren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,22 @@ void	*waiter(void *arg)
 	philo = (t_philo *)arg;
 	while (!philo->table->waiter.close_the_place)
 	{
+		pthread_mutex_lock(&(philo->table->microphone));
 		i = -1;
 		while (++i < philo->table->num_of_philos && \
 		!philo->table->waiter.close_the_place)
 		{
 			if (!is_dead(philo->table, philo[i].last_meal))
+			{
+				pthread_mutex_unlock(&(philo->table->microphone));
 				print_state(&philo[i], DIED);
+				pthread_mutex_lock(&(philo->table->microphone));
+			}
 			if (!(philo->table->waiter.table_capacity) && \
 			philo->table->times_must_eat)
 				philo->table->waiter.close_the_place = 1;
 		}
+		pthread_mutex_unlock(&(philo->table->microphone));
 		usleep(700);
 	}
 	return (NULL);
